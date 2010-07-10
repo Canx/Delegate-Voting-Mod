aa73f980e1bd4b8966413c9651548ed997ee61cf
Canx <canchete@gmail.com>
2010-07-10 17:51:57 +0200

    Eliminado fichero de backup

--- root/includes/ucp/ucp_delegate.php
+++ root/includes/ucp/ucp_delegate.php
@@ -2,7 +2,21 @@
 
 class ucp_delegate
 {
-
+   var $u_action;
+   var $new_config;
+   function main($id, $mode)
+   {
+      global $db, $user, $auth, $template;
+      global $config, $phpbb_root_path, $phpbb_admin_path, $phpEx;
+      switch($mode)
+      {
+         case 'index':
+            $this->page_title = 'UCP_DELEGATE';
+            $this->tpl_name = 'ucp_delegate';
+            break;
+      }
+ 
+   }
 }
 
 ?>
