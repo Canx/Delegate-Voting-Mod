2726bd50207430d19df0415a2cc75e2b73ee25bd
Canx <canchete@gmail.com>
2010-07-28 08:52:30 +0200

    Arreglado acceso a datos del formulario.

--- root/includes/ucp/ucp_delegate.php
+++ root/includes/ucp/ucp_delegate.php
@@ -11,43 +11,70 @@ class ucp_delegate
       
       $username	= request_var('username', '', true);
       $submit = (isset($_POST['submit'])) ? true : false;
-      
-      
-      
-      // TODO: Guardar datos nuevos
-      if ($submit) {
-         $delegado = request_var('delegado','');
-         $es_delegado = request_var('es_delegado', 0);
+      	  
+	  $sql = 'SELECT delegated_user, is_delegate
+	          FROM ' . POLL_DELEGATE_TABLE . '
+	          WHERE user_id = ' . $user->data['user_id'];   
+	  $result = $db->sql_query($sql);
+	  $fila = $db->sql_fetchrow($result);
+	  $db->sql_freeresult($result);
+		 
+	  if (!$fila) {
+	    // TODO: Si no hay resultados se crea la fila con los valores por defecto: no es delegado y no tiene delegados.
+		$sql = 'INSERT INTO ' . POLL_DELEGATE_TABLE . '
+		        SET delegated_user = null, is_delegate = 0, user_id = ' . $user->data['user_id'];
+		$db->sql_query($sql);
+			
+		$delegado_id = 0;
+		$es_delegado = 0;
+     }
+	     
+     else {
+		 $delegado_id = $fila['delegated_user'];
+		 $es_delegado = $fila['is_delegate'];
+	 }
+	 
+	 if ($delegado_id) {
+		 $sql = 'SELECT username FROM ' . USERS_TABLE . '
+		         WHERE user_id = "' . $delegado_id . '"';
+		 $result = $db->sql_query($sql);
+	     $fila = $db->sql_fetchrow($result);
+	     $db->sql_freeresult($result); 
+	      
+	     if ($fila) {
+			 $delegado = $fila['username'];
+		 }
+		 else {
+			 $delegado = '';
+		 }
+
+	 }
+		 	  
+     // TODO: Guardar datos nuevos
+     if ($submit) {
+		// TODO: Si no han cambiado los valores no hacer acceso a datos
+        $delegado = request_var('delegado','');
+        $es_delegado = request_var('es_delegado', 0);
          
-         // TODO: Guardar formulario en la base de datos.
-         //$sql = 'UPDATE 
+        if ($delegado) {
+	    	// TODO: Comprobar que el usuario tenga delegación activada.
+			$sql = 'SELECT user_id FROM ' . USERS_TABLE . '
+					WHERE username = "' . $delegado . '"';
+			$result = $db->sql_query($sql);
+			$fila = $db->sql_fetchrow($result);
+			$db->sql_freeresult($result);
+			 
+			// TODO: Controlar error si no existe el usuario.
+			if ($fila) {
+			$sql = 'UPDATE  ' . POLL_DELEGATE_TABLE . '
+					SET delegated_user = ' . $fila['user_id'] . ',
+					    is_delegate = ' . $es_delegado . ',
+					    user_id = ' . $user->data['user_id'];
+			$db->sql_query($sql);	
+			}	   
+		 }
 	  }
 	  
-	  else {
-		 $sql = 'SELECT delegated_user, is_delegate
-		         FROM ' . POLL_DELEGATE_TABLE . '
-		         WHERE user_id = ' . $user->data['user_id'];   
-		 $result = $db->sql_query($sql);
-		 $fila_delegacion = $db->sql_fetchrow($result);
-		 $db->sql_freeresult($result);
-		 
-		 if (!$fila_delegacion) {
-			// TODO: Si no hay resultados se crea la fila con los valores por defecto: no es delegado y no tiene delegados.
-			$sql = 'INSERT INTO ' . POLL_DELEGATE_TABLE . '
-			        SET delegated_user = null, is_delegate = 0, user_id = ' . $user->data['user_id'];
-			$db->sql_query($sql);
-			
-			$delegado = "";
-			$es_delegado = 0;
-	     }
-	     
-	     else {
-			 $delegado = $fila_delegacion['delegated_user'];
-			 $es_delegado = $fila_delegacion['is_delegate'];
-		 }
-		 	  
-	  }
-      
       $template->assign_vars(array(
 									'DELEGADO' => $delegado,
 									'ES_DELEGADO' => $es_delegado
